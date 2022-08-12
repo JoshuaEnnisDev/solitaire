aaaa65fd781534957c2cd874335ac3e0b54b0edd
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2022-08-11 19:18:43 -0500

    Added create deck, shuffle,some divs in DOM

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { fstat } from 'node:fs';
-
-
-let names = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-let suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
-let cards= [];
+//globals
+let playPile1, playPile2, playPile3,playPile4, playPile5, playPile6, playPile7 = [];
+let acePile1, acePile2, acePile3, acePile4 = [];
+let names = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+let suits = ["Heart", "Club", "Spade", "Diamond"];
+let cards=[];
 let card = {
     cardName: "",
     suit:"",
@@ -12,18 +12,47 @@ let card = {
     img:""
 }
 
-let imgArray = fs.readdir(singleCards)
-imgArray.forEach(image => { card.img = image
-    
-});
 
-function createDeck()
+//start helper functions
+
+function getRandomCard(array)
 {
+    let rand = Math.floor(Math.random() * (array.length));
+    return array.splice(rand, 1);
+}
+
+function shuffle()
+{
+    let shuffledDeck = [];
+
+    for (let i = 0; i < 52; i++)
+    {
+       shuffledDeck[i] = getRandomCard(cards);
+       //console.log(`the cards length is ${array.length} and i is ${i}`);
+    }
+    return shuffledDeck;
+}
+
+function drawCard(array)
+{
+    let card = array.shift();
+    array.unshift(card);
+    return card;
+}
+function placeCard(array)
+{
+    let card = array.shift();
+    return card;
+}
+
+function createDeck(array)
+{
+    
     for (let i = 0; i < suits.length; i++)
     {
         for (let j = 0; j < names.length; j++)
         {
-            newCard = Object.create(card);
+            let newCard = Object.create(card);
 
             if (i == 0 || i == 3)
             {
@@ -34,14 +63,64 @@ function createDeck()
                 newCard.color = "black";
             }
             
-            newCard.cardName = names[j];
-            newCard.suit = suits[i];
+            newCard.cardName = names[j].toUpperCase();
+            newCard.suit = suits[i].toUpperCase();
             newCard.value = j + 1;
-            cards.push(newCard);
+
+            if (newCard.value <= 10){
+                newCard.img = `singleCards/${newCard.suit}-${newCard.value}.svg`;
+            }
+            else{
+                newCard.img = `singleCards/${newCard.suit}-${newCard.value}-${newCard.cardName}.svg`;
+            }
+            
+            array.push(newCard);
             
         }
         
     }
 }
-createDeck();
-console.log(cards);
+
+function createField(size, containerClass, className)
+{
+    for (let i = 0; i < size; i++)
+    {
+        placeCard(cards);
+    }
+    
+    let container = document.querySelector(containerClass);
+    let pile = document.createElement('div');
+    pile.classList.add(className);
+    return container.appendChild(pile);
+    
+}
+
+
+/* Create 4 ace piles 
+for(i = 0; i < 4; i++) 
+{
+    createField(i, ".top", ".column");
+}
+*/
+
+/* Create and fill 7 play piles */
+for(i = 0; i < 7; i++) 
+{
+    createField(i + 1, ".middle", `.column${i + 1}`);
+    //appendCard()
+}
+
+function checkDragged()
+{
+}
+let unShuffled = createDeck(cards);
+let shuffled = shuffle(unShuffled);
+
+const holder = document.querySelector('#deck');
+
+
+
+
+
+
+
